08bbf48be377890755094f84cb5088ded17f2033
Carlos9K <carlosashraf557@gmail.com>
2024-07-25 12:06:12 +0000

    add func: device_sync_interval to handle sync logic for devices

--- erpnext_biotime/biotime_integration/biotime_integration.py
+++ erpnext_biotime/biotime_integration/biotime_integration.py
@@ -222,25 +222,51 @@ def hourly_sync_devices() -> None:
     call:
     /iclock/api/transactions/?start_time={last_activity}&end_time={last_activity+1 hours}&terminal_alias={device_alias}
     """
-    all_devices = frappe.get_all("BioTime Device", fields=["name", "device_id", "device_alias", "last_activity"])
     
+    all_devices = frappe.get_all("BioTime Device", fields=["name", "device_id", "device_alias", "last_activity","last_sync_request"])
+   
     for device in all_devices:
         all_checkins = []
-        # checkins that are not in ERPNext
         all_biotime_checkins = []
-        start_time = get_last_checkin(device)
-        end_time = (start_time + datetime.timedelta(hours=2)).strftime("%Y-%m-%d %H:%M:%S")
-        terminal_alias = device["device_alias"]
-        device_checkins, biotime_checkins = fetch_transactions(
-            start_time=start_time, end_time=end_time, terminal_alias=terminal_alias, page_size=1000
-        )        
+
+        device_checkins,biotime_checkins=device_sync_interval(device)
+
         all_checkins.extend(device_checkins)
         all_biotime_checkins.extend(biotime_checkins)
-            
+
         insert_bulk_checkins(all_checkins)
         insert_bulk_biotime_checkins(all_biotime_checkins)
+        
 
-       
+def device_sync_interval(device:dict) -> None:
+
+    end_time_now=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    h=2
+    device_id=device["device_id"]
+    while True:
+        try:
+            start_time = get_last_checkin(device)
+            end_time = (start_time + datetime.timedelta(hours=h)).strftime("%Y-%m-%d %H:%M:%S")
+            terminal_alias = device["device_alias"]
+            device_checkins, biotime_checkins = fetch_transactions(
+                start_time=start_time, end_time=end_time, terminal_alias=terminal_alias, page_size=1000
+            )        
+        
+            if len(device_checkins) == 0:
+                h*=2
+                continue     #retry the same device until !=0
+
+            if end_time>=end_time_now: # keep trying to ftech until datetime.now
+                break
+
+            logger.error(f"device ID {device_id}, hours {h} , device checkins {len(device_checkins)}")    
+
+            return device_checkins , biotime_checkins        # move for the next device
+
+        except Exception as e:
+            logger.error(f"Error syncing device ID {device_id}: {str(e)}")
+
+
 def fetch_and_insert(*args, **kwargs):
     checkins, biotime_checkins = fetch_transactions(*args, **kwargs)
 
