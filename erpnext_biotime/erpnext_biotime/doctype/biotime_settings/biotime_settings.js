7c4083852f08fb36aaf00493d5056622764edac2
Mina Botros <mina@axentor.co>
2025-12-21 16:40:40 +0000

    feat: Add biotime setting doctype

---
+++ erpnext_biotime/erpnext_biotime/doctype/biotime_settings/biotime_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Axentor and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("BioTime Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
